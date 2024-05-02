cc9e9278a91da36bdd5ab33cefe9800a879ba513
Maria Fernanda <mariasaavedra.fg@academia.umb.edu.co>
2024-05-02 20:17:27 +0000

    GUIA4

--- untitled.m
+++ untitled.m
@@ -1,32 +1,39 @@
 
 hombres2040 = 0;
 Mujeres2040 = 0;
-hombres4060 = 0;
-Mujeres4060 = 0;
+hombres4050 = 0;
+Mujeres4050 = 0;
+hombres5060 = 0;
+Mujeres5060 = 0;
 hombresmay = 0;
 Mujeresmay = 0;
 
 heart_data.age=(heart_data.age/365);
 
-% Asumimos que 'heart_data' ya está cargado y 'T' es innecesario
 for i = 1:length(heart_data.age)
-    if heart_data.cardio(i) == 1 % Solo consideramos casos con enfermedad cardiovascular
-        % Menores de 20 años
+    if heart_data.cardio(i) == 1 
+       
         if heart_data.age(i) <= 40
             if heart_data.gender(i) == 1
                 hombres2040 = hombres2040 + 1;
             elseif heart_data.gender(i) == 2
                 Mujeres2040 = Mujeres2040 + 1;
             end
-        % Entre 40 y 60 años
-        elseif heart_data.age(i) <= 60
+        
+        elseif heart_data.age(i) > 40 && heart_data.age(i) <= 50
             if heart_data.gender(i) == 1
-                hombres4060 = hombres4060 + 1;
+                hombres4050 = hombres4050 + 1;
             elseif heart_data.gender(i) == 2
-                Mujeres4060 = Mujeres4060 + 1;
+                Mujeres4050 = Mujeres4050 + 1;
             end
-        % Mayores de 60 años
-        else
+        %
+        elseif heart_data.age(i) > 50 && heart_data.age(i) <= 60
+            if heart_data.gender(i) == 1
+                hombres5060 = hombres5060 + 1;
+            elseif heart_data.gender(i) == 2
+                Mujeres5060 = Mujeres5060 + 1;
+            end
+        elseif heart_data.age(i) > 60
             if heart_data.gender(i) == 1
                 hombresmay = hombresmay + 1;
             elseif heart_data.gender(i) == 2
@@ -37,8 +44,8 @@ for i = 1:length(heart_data.age)
 end
 
 % Datos para la gráfica
-x = [ "entre 20 y 40 años", "entre 40 y 60 años", "más de 60 años"];
-y = [hombres2040 Mujeres2040; hombres4060 Mujeres4060; hombresmay Mujeresmay];
+x = [ "Menor a 40", "entre 40 y 50 años","entre 50 y 60 años", "más de 60 años"];
+y = [hombres2040 Mujeres2040; hombres4050 Mujeres4050; hombres5060 Mujeres5060;hombresmay Mujeresmay];
 bar(x, y);
 xlabel('Edad de las personas');
 ylabel('Cantidad de personas');
